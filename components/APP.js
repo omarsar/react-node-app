1b5fb2d354f4ea5a3c8ecfff86a852e04cab9a69
Elvis Saravia <ellfae@gmail.com>
2015-09-20 18:17:42 +0800

    sdfsdfsd

--- components/APP.js
+++ components/APP.js
@@ -22,8 +22,8 @@ var APP = React.createClass({
 	componentWillMount(){
 		//TODO: need to fix automatic environment settings
 		//(process.env.NODE_ENV === 'production') ? this.socket = io('https://fathomless-sea-2599.herokuapp.com') : this.socket = io('http://localhost:5000');
-		//this.socket = io('http://localhost:5000');
-		this.socket = io('https://fathomless-sea-2599.herokuapp.com')
+		this.socket = io('http://localhost:5000');
+		//this.socket = io('https://fathomless-sea-2599.herokuapp.com')
 		this.socket.on('connect', this.connect);
 		this.socket.on('disconnect', this.disconnect);
 		this.socket.on('welcome', this.updateState);
